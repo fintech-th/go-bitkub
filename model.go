2795d34c8552a0ee17d8a0f4963ef24eb52f698f
Tanapon Suwanmanee <swmn.tanapon@gmail.com>
2022-07-12 10:41:03 +0700

    refactor: chang field to private

--- model.go
+++ model.go
@@ -66,8 +66,8 @@ type OrderHistoryBody struct {
 }
 
 type ListOrderHistoryRequest struct {
-	Sig       string `json:"sig,omitempty"`
-	Timestamp string `json:"ts"`
+	sig       string `json:"sig,omitempty"`
+	timestamp string `json:"ts"`
 	Symbol    string `json:"sym"`
 	Start     int    `json:"start,omitempty"`
 	End       int    `json:"end,omitempty"`
